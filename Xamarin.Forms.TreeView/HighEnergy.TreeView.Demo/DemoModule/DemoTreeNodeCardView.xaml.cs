f593cf7f2da9b481cc75c4447c1c355ee7d861e4
Randy Chapman <randyc@microsoft.com>
2016-08-22 07:39:54 -0700

    Bind the spacing element instead of adjusting it in code (in the demo)

--- Xamarin.Forms.TreeView/HighEnergy.TreeView.Demo/DemoModule/DemoTreeNodeCardView.xaml.cs
+++ Xamarin.Forms.TreeView/HighEnergy.TreeView.Demo/DemoModule/DemoTreeNodeCardView.xaml.cs
@@ -9,47 +9,9 @@ namespace HighEnergy.TreeView.Demo
 {
     public partial class DemoTreeCardView : ContentView
     {
-        DemoTreeNode Node { get; set; }
-
         public DemoTreeCardView()
         {
             InitializeComponent();
-
-            Debug.WriteLine("new DemoTreeCardView");
-
-            // TODO: replace this mechanism with binding to Width, if possible
-            SizeChanged += (sender, e) => AdjustSpacer();
-        }
-
-        protected override void OnBindingContextChanged()
-        {
-            base.OnBindingContextChanged();
-
-            var newNode = BindingContext as DemoTreeNode;
-            if (newNode == null)
-            {
-                if (Node != null)
-                {
-                    Debug.WriteLine("OnBindingContextChanged: Node.PropertyChanged unsubscribe");
-                    Node.PropertyChanged -= (sender, e) => AdjustSpacer();
-                }
-
-                Spacer.WidthRequest = 0;
-                return;
-            }
-
-            Node = BindingContext as DemoTreeNode;
-            if (Node != null)
-            {
-                Debug.WriteLine("OnBindingContextChanged: Node.PropertyChanged subscribe");
-                Node.PropertyChanged += (sender, e) => AdjustSpacer();
-            }
-        }
-
-        void AdjustSpacer()
-        {
-            if (Node != null)
-                Spacer.WidthRequest = Node.IndentWidth;
         }
     }
 }
